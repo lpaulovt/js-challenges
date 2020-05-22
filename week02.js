ee4cf29ece21d1ffe450b2b809f996058b21f4c7
Paulo Lopes <silvavitorlopes711@gmail.com>
2020-05-22 19:44:31 -0300

    add: challenge04.02 - week 02

--- week02.js
+++ week02.js
@@ -85,3 +85,7 @@ function elseif(x,y,z){
         return null
     }
 }
+
+// Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
+console.log(elseif())
+//false
